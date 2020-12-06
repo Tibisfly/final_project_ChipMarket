23b7b9c2c6d0aaaef1bf7796ea5131795b923847
Tibisfly <vielmatorotibisay@gmail.com>
2020-12-06 00:10:11 +0000

    He hecho commerces, posts, followers, likes y comments sin probar en insomnia por la duda

---
+++ src/api/api.md
@@ -0,0 +1,24 @@
+# Users
+
+Esta es nuestra colección de usuarios
+
+## List users 
+
+## Request es con un:
+
+```
+GET/users/<int:id>
+```
+
+### Response, si es un sólo diccionario no es necesario colocar los [] de listas. 
+```
+{
+    "id": 1,
+    "first_name": "Enrique",
+    "last_name": "Rodríguez",
+    "email": "kirodriguezke@gmail.com",
+    "username": "KirodriguezKe,
+    "password": "ecosistema",
+    "avatar": "url",      
+}
+
--- src/api/routes.py
+++ src/api/routes.py
@@ -29,7 +29,8 @@ def handle_get_user(id):
     return jsonify(user1.serialize()), 200
 
 
-#Se crea un usario nuevo o cualquier cosa nueva que se añada en la base de datos.
+#Se crea un usario nuevo o cualquier cosa nueva que se añada en la base de datos. 
+# AQUÍ CREO QUE NO VA TODAVÍA EL <int:id> porque se debe genrar es después de que se haga el POST.
 @api.route('/users/<int:id>', methods=['POST'])
 def handle_create_user():
     payload = request.get_json()
@@ -48,3 +49,102 @@ def handle_update_user(id):
 def handle_delete_user(id):
     
     return "User #{} updated.".format(id)
+
+### COMMERCES ###
+
+#Creación del commerce
+# ASOCIA EL USUARIO A UN NUEVO COMERCIO, ese <int:id> es el id del usuario, pero con el post se crea el owner_id
+@api.route('/commerces/<int:id_user>', methods=['POST'])
+def handle_create_commerce():
+    payload = request.get_json()
+    print(payload)
+    return "Create Commerce and put as owner_id the user_id"
+
+#Obtener la lista de todos los comercios, aquí mi duda es si en el endpoint lleva el <int:id>
+@api.route('/commerces/<int:id>', methods=['GET'])
+def handle_list_commerces():
+    return "List all commerces."
+
+#Devuelve el comercio que se busca.
+@api.route('/commerces/<int:id>', methods=['GET'])
+def handle_get_commerces(id):
+    commerce1 = commerces.query.get(id)
+    return jsonify(commerce1.serialize()), 200
+
+#Se actualiza un comercio ya creado.
+@api.route('/commerces/<int:id>', methods=['PUT'])
+def handle_update_commerce(id):
+    payload = request.get_json()
+    print(payload)
+    return "Commerce #{} updated.".format(id)
+
+#Borrar un comercio.
+@api.route('/commerces/<int:id>', methods=['DELETE'])
+def handle_delete_commerce(id):
+    
+    return "Commerce #{} updated.".format(id)
+
+### POSTS ###
+
+#Creación del POST, recordemos que sólo lo puede hacer el commerce
+#Si estamos logeados como commerce no debería hacer falta pasar el id (porque ya sabemos quién es el que hace el post)
+#Si es un usuario debe dar error 404 (seguro habrá que hacer un if)
+@api.route('/posts/<int:id>', methods=['POST'])
+def handle_create_posts():
+    payload = request.get_json()
+    print(payload)
+    return "Create Post"
+
+#Obtener la lista de todos los posts, aquí mi duda es si en el endpoint lleva el <int:id> (al igual que en los de arriba)
+@api.route('/posts/<int:id>', methods=['GET'])
+def handle_list_posts():
+    return "List all Posts of the commerces."
+
+#Devuelve el post del comercio que se busca.
+@api.route('/posts/<int:id>', methods=['GET'])
+def handle_get_posts(id):
+    post1 = posts.query.get(id)
+    return jsonify(post1.serialize()), 200
+
+#Se actualiza un post ya creado.
+@api.route('/posts/<int:id>', methods=['PUT'])
+def handle_update_posts(id):
+    payload = request.get_json()
+    print(payload)
+    return "Post #{} updated.".format(id)
+
+#Borrar un post.
+@api.route('/posts/<int:id>', methods=['DELETE'])
+def handle_delete_posts(id):
+    
+    return "Post #{} updated.".format(id)
+
+### FOLLOWERS ###
+
+#Acción de seguir, no entiendo muy bien como relacionar ambos id (user+commerce)
+@api.route('/followers/<int:id_commerce>/<int:id_user>', methods=['POST'])
+def handle_create_followers(id_commerce, id_user):
+    payload = request.get_json()
+    print(payload)
+    return "Follow a commerce"
+
+#Obtener la lista de todos los followers de un comercio? No se como hacer con ese id
+@api.route('/followers/<int:id>', methods=['GET'])
+def handle_list_followers():
+    return "List of all followes."
+
+#Devuelve el follower específico que quieras buscar.
+#Esto sería si quiero ver algún follower en específico?
+@api.route('/followers/<int:id>', methods=['GET'])
+def handle_get_followers(id):
+    follower1 = followers.query.get(id)
+    return jsonify(follower1.serialize()), 200
+
+#Dejar de seguir. 
+@api.route('/followers/<int:id>', methods=['DELETE'])
+def handle_delete_followers(id):
+    
+    return "follower #{} deleted.".format(id)
+
+
+
