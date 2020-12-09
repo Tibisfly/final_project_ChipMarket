38b2c384c250ff829919a1b0d8ee635f35053cd6
Tibisfly <vielmatorotibisay@gmail.com>
2020-12-09 16:23:16 +0000

    Relación entre Users y Commerce comprobadas, mentoría con Sergio. models.py

--- src/api/models.py
+++ src/api/models.py
@@ -16,12 +16,12 @@ class Users(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     avatar = db.Column(db.String(80))
 
-    commerce = db.relationship("Commerces")
-    follower = db.relationship("Followers")
-    like = db.relationship("Likes")
-    comment = db.relationship("Comments")
-def __str__(self):
-    return  '{}: {}.' .format(self.user.first_name, self.email,)
+    commerce = db.relationship("Commerces", backref='users')
+    # follower = db.relationship("Followers")
+    # like = db.relationship("Likes")
+    # comment = db.relationship("Comments")
+    # def __str__(self):
+    #     return  '{}: {}.' .format(self.user.first_name, self.email,)
 
 
 
@@ -45,25 +45,25 @@ class Commerces(db.Model):
     website = db.Column(db.String(300))
     phone_number = db.Column(db.String(12))
 
-    user = db.relationship("Users")
-    follower = db.relationship("Followers")
-    post = db.relationship("Posts")
+    # user = db.relationship("Users")
+    # follower = db.relationship("Followers")
+    # post = db.relationship("Posts")
 
 class Followers(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     created_at = db.Column(db.DateTime, server_default=func.now())
     updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
     deleted_at = db.Column(db.DateTime)
-    commerce_id = db.Column(db.Integer, db.ForeignKey('commerces.id'),  nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'),  nullable=False)
+    # commerce_id = db.Column(db.Integer, db.ForeignKey('commerces.id'),  nullable=False)
+    # user_id = db.Column(db.Integer, db.ForeignKey('users.id'),  nullable=False)
 
-    user = db.relationship("Users")
-    commerce = db.relationship("Commerces")
+    # user = db.relationship("Users")
+    # commerce = db.relationship("Commerces")
 
 
 class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    commerce_id = db.Column(db.Integer, db.ForeignKey('commerces.id'), nullable=False)
+    # commerce_id = db.Column(db.Integer, db.ForeignKey('commerces.id'), nullable=False)
     created_at = db.Column(db.DateTime, server_default=func.now())
     updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
     deleted_at = db.Column(db.DateTime)
@@ -72,32 +72,32 @@ class Posts(db.Model):
     title = db.Column(db.String(80), nullable=False)
     description = db.Column(db.Text, nullable=True)
 
-    commerce = db.relationship("Commerces")
-    comment = db.relationship("Comments")
-    like = db.relationship("Likes")
+    # commerce = db.relationship("Commerces")
+    # comment = db.relationship("Comments")
+    # like = db.relationship("Likes")
 
 class Likes(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
+    # user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    # post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
     created_at = db.Column(db.DateTime, server_default=func.now())
     updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
     deleted_at = db.Column(db.DateTime)
     
-    user = db.relationship("Users")
-    post = db.relationship("Posts")
+    # user = db.relationship("Users")
+    # post = db.relationship("Posts")
     
 class Comments(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    # user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    # post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
     created_at = db.Column(db.DateTime, server_default=func.now())
     updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
     deleted_at = db.Column(db.DateTime)
     text = db.Column(db.Text, nullable=False)
     
-    user = db.relationship("Users")
-    post = db.relationship("Posts")
+    # user = db.relationship("Users")
+    # post = db.relationship("Posts")
 
 
 
