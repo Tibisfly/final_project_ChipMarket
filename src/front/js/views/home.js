091d55a24e29a7885061fcd4de8430ecea3d4602
Tibisfly <vielmatorotibisay@gmail.com>
2021-01-21 21:46:42 +0000

    haciendo el push antes de que me cargue el carousel

--- src/front/js/views/home.js
+++ src/front/js/views/home.js
@@ -1,27 +1,81 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import { Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption } from "reactstrap";
 
 export const Home = () => {
+	const items = [
+		{
+			src:
+				"https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80",
+			altText: "Slide 1",
+			caption: "Slide 1"
+		},
+		{
+			src:
+				"https://images.unsplash.com/photo-1509803992307-5ed1ac32efda?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+			altText: "Slide 2",
+			caption: "Slide 2"
+		},
+		{
+			src:
+				"https://images.unsplash.com/photo-1603056724920-fdecbb3b7d37?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+			altText: "Slide 3",
+			caption: "Slide 3"
+		}
+	];
+
+	const [activeIndex, setActiveIndex] = useState(0);
+	const [animating, setAnimating] = useState(false);
+
+	const next = () => {
+		if (animating) return;
+		const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+		setActiveIndex(nextIndex);
+	};
+
+	const previous = () => {
+		if (animating) return;
+		const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+		setActiveIndex(nextIndex);
+	};
+
+	const goToIndex = newIndex => {
+		if (animating) return;
+		setActiveIndex(newIndex);
+	};
+
+	const slides = items.map(item => {
+		return (
+			<CarouselItem onExiting={() => setAnimating(true)} onExited={() => setAnimating(false)} key={item.src}>
+				<img src={item.src} alt={item.altText} />
+				<CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+			</CarouselItem>
+		);
+	});
+
 	const { store, actions } = useContext(Context);
 
 	return (
-		<>
-			<section className="py-3">
-				<div className="container">
-					<div className="carousel-item active">
-						<h1 className="text-center my-5" id="home-title">
-							Ahorra tiempo y dinero
-							<br />
-							mientras aportas un grano de arena
-						</h1>
+		<div className="container ">
+			<Carousel activeIndex={activeIndex} next={next} previous={previous}>
+				<CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+				{slides}
+				<CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+				<CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+			</Carousel>
+			<br />
+			<div className="card my-3 border-0">
+				<div className="row g-0">
+					<div className="col-md-3 d-flex justify-content-center">
+						<img
+							src="https://images.unsplash.com/photo-1585776460744-d32ffc487b3e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE1fHx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+							alt="..."
+							className="rounded-circle"
+							style={{ width: "85%", height: "100%" }}
+						/>
 					</div>
-					<img
-						src="https://images.unsplash.com/photo-1584937248081-634bb38393fc?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80"
-						className="img-home d-block w-100"
-					/>
-					<br />
 					<div className="col-md-8">
 						<div className="card-body">
 							<h5 className="card-title">AHORRA TIEMPO Y DINERO</h5>
@@ -35,7 +89,7 @@ export const Home = () => {
 						</div>
 					</div>
 				</div>
-			</section>
-		</>
+			</div>
+		</div>
 	);
 };
