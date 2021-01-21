18544400ace0282e95e794863e432a0f270ac0f0
Tibisfly <vielmatorotibisay@gmail.com>
2021-01-21 22:09:30 +0000

    me esta fallando el gitpod

--- src/front/js/views/home.js
+++ src/front/js/views/home.js
@@ -66,27 +66,43 @@ export const Home = () => {
 				<CarouselControl direction="next" directionText="Next" onClickHandler={next} />
 			</Carousel>
 			<br />
-			<div className="card my-3 border-0">
+			<div className="card my-5 border-0">
 				<div className="row g-0">
-					<div className="col-md-3 d-flex justify-content-center">
+					<div className="col-md-4 d-flex justify-content-center">
 						<img
-							src="https://images.unsplash.com/photo-1585776460744-d32ffc487b3e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE1fHx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+							src="https://images.unsplash.com/photo-1580361543250-301150bf6551?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8M3w5NjgzNjM5fHxlbnwwfHx8&auto=format&fit=crop&w=500&q=60"
 							alt="..."
 							className="rounded-circle"
-							style={{ width: "85%", height: "100%" }}
+							style={{ width: "100%", height: "auto" }}
 						/>
 					</div>
+					<div className="col-md-8 my-5">
+						<div className="card-body">
+							<h5 className="card-title">UNA REALIDAD QUE PODEMOS MEJORAR JUNTOS.</h5>
+							<p className="card-text text-justify">
+								No es secreto que el año 2020 fue duro para todos, pero aún más para personas muy
+								cercanas a ti: El comerciante de barrio. Fruterías, ferreterías, restaurantes,
+								peluquerías y variedad de comercios se han visto afectados por el confinamiento del
+								2020. Más del 5% de comercios de barrio han cerrado en España por motivo al coronavirus.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<br />
+			<div className="card mb-3 border-0 my-5">
+				<div className="row g-0">
 					<div className="col-md-8">
 						<div className="card-body">
-							<h5 className="card-title">AHORRA TIEMPO Y DINERO</h5>
+							<h5 className="card-title">AHORRA TIEMPO Y DINERO 2222</h5>
 							<p className="card-text">
 								Al alcance de un click podrás conseguir las mejores ofertas cerca de ti. Introduce el
 								código postal de la zona y disfruta de los beneficios de ser ChipAddict
 							</p>
-							<p className="card-text">
-								<small className="text-muted">Last updated 3 mins ago</small>
-							</p>
 						</div>
+					</div>
+					<div className="col-md-4 rounded-pill">
+						<img src="https://i.pinimg.com/236x/c0/73/a5/c073a5bf67337759411f80aad9588d58.jpg" alt="..." />
 					</div>
 				</div>
 			</div>
