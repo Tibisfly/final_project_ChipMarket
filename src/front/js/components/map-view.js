2f24e2ddae488fb012d9cb4147e03b9e9e56446a
Tibisfly <vielmatorotibisay@gmail.com>
2021-02-18 04:08:06 +0000

    maps markers done

--- src/front/js/components/map-view.js
+++ src/front/js/components/map-view.js
@@ -49,39 +49,86 @@ const MapTest = () => {
 
 	return (
 		<div className="map-test">
-			<MapContainer center={{ lat: 40.42678727667202, lng: -3.6672587479438 }} zoom={20} scrollWheelZoom={false}>
+			<MapContainer center={{ lat: 40.42678727667202, lng: -3.6672587479438 }} zoom={17} scrollWheelZoom={false}>
 				<TileLayer
 					attribution='&copy; <a href="https://www.maptiler.com/">MapTiler</a> &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 					url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=wHKJGX6RktBjxZuy5hCf"
 				/>
 				<Marker position={{ lat: 40.42678727667202, lng: -3.6672587479438 }} icon={markerIcon}>
 					<Popup>
-						<b>Ferretería Ayala</b>
+						<b>Ferretería San Diego.</b>
+						<br />
+						<b>Paseo Marqués de Zafra, 9. Madrid</b>
 					</Popup>
 				</Marker>
 				<Marker position={{ lat: 40.426785832905, lng: -3.668250674020496 }} icon={markerIcon}>
 					<Popup>
-						<b>Panarte</b>
+						<b>Panadería Panarte.</b>
+						<br />
+						<b>Paseo Marqués de Zafra, 2. Madrid</b>
 					</Popup>
 				</Marker>
-				<Marker position={{ lat: 40.426583843681705, lng: -3.667354746751704 }} icon={markerIcon}>
+				<Marker position={{ lat: 40.42579783696785, lng: -3.6635803495832144 }} icon={markerIcon}>
 					<Popup>
-						<b>Pollería</b>
+						<b>Ay Madre, La Fruta.</b>
+						<br />
+						<b>Paseo Marques de Zafra 39. Madrid</b>
+					</Popup>
+				</Marker>
+				<Marker position={{ lat: 40.42536300300019, lng: -3.665367014001046 }} icon={markerIcon}>
+					<Popup>
+						<b>Pollera Santa Paca.</b>
+						<br />
+						<b>Paseo Marques de Zafra 16. Madrid</b>
+					</Popup>
+				</Marker>
+				<Marker position={{ lat: 40.42538750501744, lng: -3.665383107254526 }} icon={markerIcon}>
+					<Popup>
+						<b>SBH Peluquería Unisex</b>
+						<br />
+						<b>Calle Lanuza, 7. Madrid</b>
+					</Popup>
+				</Marker>
+				<Marker position={{ lat: 40.42679541886236, lng: -3.6656256360901383 }} icon={markerIcon}>
+					<Popup>
+						<b>Papelería Borrego.</b>
+						<br />
+						<b>Calle Rafael Juan y Seva, 5. Madrid</b>
+					</Popup>
+				</Marker>
+				<Marker position={{ lat: 40.411219737038834, lng: -3.694624972985061 }} icon={markerIcon}>
+					<Popup>
+						<b>El Florista</b>
+						<br />
+						<b>Calle Almadén 12. Madrid</b>
+					</Popup>
+				</Marker>
+				<Marker position={{ lat: 40.42642186341579, lng: -3.663984186477782 }} icon={markerIcon}>
+					<Popup>
+						<b>Talleres Díaz Auto.</b>
+						<br />
+						<b>Calle del Vizconde de Matamala, 14. Madrid</b>
+					</Popup>
+				</Marker>
+				<Marker position={{ lat: 40.42670859512874, lng: -3.667701357642063 }} icon={markerIcon}>
+					<Popup>
+						<b>El Perro Gamberro. Tabernita</b>
+						<br />
+						<b>Calle Benito de Castro, nº 4. Madrid</b>
+					</Popup>
+				</Marker>
+				<Marker position={{ lat: 40.42857068580306, lng: -3.6667143046709185 }} icon={markerIcon}>
+					<Popup>
+						<b>Pinturas Pami.</b>
+						<br />
+						<b>Calle Rufino Blanco, 7. Madrid</b>
 					</Popup>
 				</Marker>
 				<Marker position={{ lat: 40.42622040262896, lng: -3.6660833796771173 }} icon={markerIcon}>
 					<Popup>
-						<b>Abedul Celinda</b>
-					</Popup>
-				</Marker>
-				<Marker position={{ lat: 40.42834980654245, lng: -3.6655819712427915 }} icon={markerIcon}>
-					<Popup>
-						<b>Grafica Tirado S.L.</b>
-					</Popup>
-				</Marker>
-				<Marker position={{ lat: 40.42834980654245, lng: -3.6667500732679974 }} icon={markerIcon}>
-					<Popup>
-						<b>Pinturas Pami</b>
+						<b>Pollera Santa Paca.</b>
+						<br />
+						<b>Paseo Marques de Zafra 2. Madrid</b>
 					</Popup>
 				</Marker>
 			</MapContainer>
--- src/front/js/views/search-zipcode.js
+++ src/front/js/views/search-zipcode.js
@@ -20,10 +20,6 @@ export const SearchZipCode = function(props) {
 					<div className="col-lg-6 col-12">
 						<div className="title-search-zipcode">
 							<img className="logo-container-login-commerce" src={Hands} />
-							{/* <p>
-								Sigue a los comercios más cerca de ti. Reclama tu oferta en el local con el código
-								promocional que encontrarás en cada publicación.
-							</p> */}
 						</div>
 					</div>
 					<div className="col-lg-6">
@@ -36,83 +32,152 @@ export const SearchZipCode = function(props) {
 				<div className="row no-gutters">
 					<div className="col-4">
 						<Link to={`/feed/commerce/1`}>
-							<img
-								src="https://i2.wp.com/amasa.es/wp-content/uploads/2018/03/las_panaderias_han_vuelto.jpg?fit=800%2C600&ssl=1"
-								className="card-img-top"
-								alt="imagen1"
-							/>
+							<div className="img__wrap">
+								<img
+									src="https://i2.wp.com/amasa.es/wp-content/uploads/2018/03/las_panaderias_han_vuelto.jpg?fit=800%2C600&ssl=1"
+									className="card-img-top"
+									alt="imagen1"
+								/>
+								<div className="img__description_layer">
+									<p className="img__description search-name">Panadería Panarte.</p>
+									<p className="img__description search-address">Paseo Marques de Zafra 2. Madrid</p>
+									<p className="img__description search-offers">3 ofertas activas.</p>
+								</div>
+							</div>
 						</Link>
 					</div>
 					<div className="col-4">
 						<Link to={`/feed/commerce/2`}>
-							<img
-								src="https://www.mercadodelacebada.com/wp-content/uploads/2018/03/MercadoDeLaCebada_POLLERIA-ANA.jpg"
-								className="card-img-top"
-								alt="imagen2"
-							/>
+							<div className="img__wrap">
+								<img
+									src="https://www.mercadodelacebada.com/wp-content/uploads/2018/03/MercadoDeLaCebada_POLLERIA-ANA.jpg"
+									className="card-img-top"
+									alt="imagen2"
+								/>
+								<div className="img__description_layer">
+									<p className="img__description search-name">Pollera Santa Paca.</p>
+									<p className="img__description search-address">Paseo Marques de Zafra 16. Madrid</p>
+									<p className="img__description search-offers">3 ofertas activas.</p>
+								</div>
+							</div>
 						</Link>
 					</div>
 					<div className="col-4">
 						<Link to={`/feed/commerce/3`}>
-							<img
-								src="https://www.lomejordelbarrio.com/image/800/600/files/uploads/file/6e2a3f01-f80e-4705-970c-b155690a6551/ferreteria-san-diego-puentevallecas-fachada1.jpg"
-								className="card-img-top"
-								alt="imagen3"
-							/>
+							<div className="img__wrap">
+								<img
+									src="https://www.lomejordelbarrio.com/image/800/600/files/uploads/file/6e2a3f01-f80e-4705-970c-b155690a6551/ferreteria-san-diego-puentevallecas-fachada1.jpg"
+									className="card-img-top"
+									alt="imagen3"
+								/>
+								<div className="img__description_layer">
+									<p className="img__description search-name">Ferretería San Diego.</p>
+									<p className="img__description search-address">Paseo Marques de Zafra 9. Madrid</p>
+									<p className="img__description search-offers">5 ofertas activas.</p>
+								</div>
+							</div>
 						</Link>
 					</div>
 					<div className="col-4">
 						<Link to={`/feed/commerce/4`}>
-							<img
-								src="https://ep00.epimg.net/elpais/imagenes/2020/09/12/album/1599902515_614526_1599904676_album_normal.jpg"
-								className="card-img-top"
-								alt="imagen4"
-							/>
+							<div className="img__wrap">
+								<img
+									src="https://ep00.epimg.net/elpais/imagenes/2020/09/12/album/1599902515_614526_1599904676_album_normal.jpg"
+									className="card-img-top"
+									alt="imagen4"
+								/>
+								<div className="img__description_layer">
+									<p className="img__description search-name">Ay Madre, La Fruta</p>
+									<p className="img__description search-address">Paseo Marques de Zafra 39. Madrid</p>
+									<p className="img__description search-offers">4 ofertas activas.</p>
+								</div>
+							</div>
 						</Link>
 					</div>
 					<div className="col-4">
 						<Link to={`/feed/commerce/5`}>
-							<img
-								src="https://ak.uecdn.es/p/108/thumbnail/entry_id/0_yh7d9ayn/width/600/"
-								className="card-img-top"
-								alt="imagen5"
-							/>
+							<div className="img__wrap">
+								<img
+									src="https://ak.uecdn.es/p/108/thumbnail/entry_id/0_yh7d9ayn/width/600/"
+									className="card-img-top"
+									alt="imagen5"
+								/>
+								<div className="img__description_layer">
+									<p className="img__description search-name">SBH Peluquería Unisex</p>
+									<p className="img__description search-address">Calle Lanuza 7.</p>
+									<p className="img__description search-offers">4 ofertas activas.</p>
+								</div>
+							</div>
 						</Link>
 					</div>
 					<div className="col-4">
 						<Link to={`/feed/commerce/6`}>
-							<img
-								src="https://t2.salir.ltmcdn.com/es/places/2/7/8/img_44872_pinturas-borreguero_0_orig.jpg"
-								className="card-img-top"
-								alt="imagen6"
-							/>
+							<div className="img__wrap">
+								<img
+									src="https://t2.salir.ltmcdn.com/es/places/2/7/8/img_44872_pinturas-borreguero_0_orig.jpg"
+									className="card-img-top"
+									alt="imagen6"
+								/>
+								<div className="img__description_layer">
+									<p className="img__description search-name">Papelería Borrego</p>
+									<p className="img__description search-address">
+										Local, Calle Rafael Juan y Seva, 5. Madrid
+									</p>
+									<p className="img__description search-offers">3 ofertas activas.</p>
+								</div>
+							</div>
 						</Link>
 					</div>
 					<div className="col-4">
 						<Link to={`/feed/commerce/7`}>
-							<img
-								src="https://media.timeout.com/images/105251564/image.jpg"
-								className="card-img-top"
-								alt="imagen7"
-							/>
+							<div className="img__wrap">
+								<img
+									src="https://media.timeout.com/images/105251564/image.jpg"
+									className="card-img-top"
+									alt="imagen7"
+								/>
+								<div className="img__description_layer">
+									<p className="img__description search-name">El Florista</p>
+									<p className="img__description search-address">Calle de Almadén, 12. Madrid</p>
+									<p className="img__description search-offers">6 ofertas activas.</p>
+								</div>
+							</div>
 						</Link>
 					</div>
 					<div className="col-4">
 						<Link to={`/feed/commerce/8`}>
-							<img
-								src="https://static.miweb.padigital.es/var/m_8/88/88e/64988/950003-734149-img5.jpg"
-								className="card-img-top"
-								alt="imagen8"
-							/>
+							<div className="img__wrap">
+								<img
+									src="https://static.miweb.padigital.es/var/m_8/88/88e/64988/950003-734149-img5.jpg"
+									className="card-img-top"
+									alt="imagen8"
+								/>
+								<div className="img__description_layer">
+									<p className="img__description search-name">Talleres Díaz Auto</p>
+									<p className="img__description search-address">
+										Calle del Vizconde de Matamala, 14. Madrid
+									</p>
+									<p className="img__description search-offers">2 ofertas activas.</p>
+								</div>
+							</div>
 						</Link>
 					</div>
 					<div className="col-4">
 						<Link to={`/feed/commerce/9`}>
-							<img
-								src="http://tabernalagaditana.com/wp-content/uploads/2013/11/solea1.jpg"
-								className="card-img-top"
-								alt="imagen9"
-							/>
+							<div className="img__wrap">
+								<img
+									src="http://tabernalagaditana.com/wp-content/uploads/2013/11/solea1.jpg"
+									className="card-img-top"
+									alt="imagen9"
+								/>
+								<div className="img__description_layer">
+									<p className="img__description search-name">El Perro Gamberro. Tabernita</p>
+									<p className="img__description search-address">
+										Calle Benito de Castro, nº 4. Madrid
+									</p>
+									<p className="img__description search-offers">3 ofertas activas.</p>
+								</div>
+							</div>
 						</Link>
 					</div>
 				</div>
